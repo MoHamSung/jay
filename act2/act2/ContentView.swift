66dc379d870cc19716a4f0d5cf5cd774c3d02750
jay1261 <dongjae73@naver.com>
2023-05-21 22:31:48 +0900

    [Init] - act2 init

---
+++ act2/act2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  act2
+//
+//  Created by Jay on 2023/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ act2/act2/act2App.swift
@@ -0,0 +1,17 @@
+//
+//  act2App.swift
+//  act2
+//
+//  Created by Jay on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct act2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
