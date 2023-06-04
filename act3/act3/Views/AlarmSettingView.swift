432cdf590f81759035d43861990136f264a80811
jay1261 <dongjae73@naver.com>
2023-06-04 15:00:51 +0900

    [Feat] - AlarmSettingView

--- act3/act3/Views/AlarmSettingView.swift
+++ act3/act3/Views/AlarmSettingView.swift
@@ -8,24 +8,87 @@
 import SwiftUI
 
 struct AlarmSettingView: View {
+    @Binding var isPlusPressed: Bool
+    
     var body: some View {
         VStack{
-// TODO: - 텍스트 칸 잡아먹는 거 해결해야 함. -> 해결?
+            // TODO: - 텍스트 칸 잡아먹는 거 해결해야 함. -> 해결?
             DatePicker("김말티 바보", selection: .constant(.now), displayedComponents: .hourAndMinute)
                 .datePickerStyle(.wheel)
                 .labelsHidden()
-         
+            
             List {
-                Text("반복")
+                AlarmOptionView(title: "반복") {
+                    Text("안함")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("ColorFontGray"))
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color("ColorFontGray"))
+                }
+                AlarmOptionView(title: "레이블") {
+                    Text("알람")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("ColorFontDarkGray"))
+                }
+                AlarmOptionView(title: "레이블") {
+                    Text("녹차")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("ColorFontGray"))
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color("ColorFontGray"))
+                }
+                AlarmOptionView(title: "다시 알림") {
+                    Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    }
+                }
             }
         }
-        
+        .navigationTitle(Text("알람 추가").font(.system(size: 16,weight: .bold)))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction){
+                Button {
+                    isPlusPressed.toggle()
+                }label: {
+                    Text("취소")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(Color("ColorFontOrange"))
+                }
+            }
+            ToolbarItem(placement: .confirmationAction){
+                Button {
+                    isPlusPressed.toggle()
+                }label: {
+                    Text("저장")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(Color("ColorFontOrange"))
+                }
+            }
+        }
+    }
+}
+
+struct AlarmOptionView<Content>: View where Content: View {
+    let title: String
+    @ViewBuilder let content: Content
+    
+    var body: some View {
+        HStack(spacing: 5) {
+            Text(title)
+                .font(.system(size: 15))
+            Spacer()
+            content
+        }
     }
 }
 
 struct AlarmSettingView_Previews: PreviewProvider {
     static var previews: some View {
-        AlarmSettingView()
-            .preferredColorScheme(.dark)
+        NavigationView{
+            AlarmSettingView(isPlusPressed: .constant(true))
+                .preferredColorScheme(.dark)
+        }
     }
 }
--- act3/act3/Views/MainView.swift
+++ act3/act3/Views/MainView.swift
@@ -17,26 +17,69 @@ struct MainView: View {
 }
 
 struct NavigationBar: View{
+    @State var isPlusPressed = false
+    
     var body: some View {
         List {
-            Section(header: Text("수면 | 기상")){
-                Text("asdf")
-                Text("asdf")
-            }
-            
-            Section(header: Text("기타")){
+            // 수면|기상 ~ 알람없음까지
+            Section(
+                header:
+                    HStack(spacing:0){
+                        Text("\(Image(systemName: "bed.double.fill"))")
+                            .font(.system(size: 14))
+                            .foregroundColor(Color("ColorFontWhite"))
+                        Text("수면 | 기상")
+                            .font(.system(size: 17))
+                            .foregroundColor(Color("ColorFontWhite"))
+                    }){
+                        HStack{
+                            Text("알람 없음")
+                                .font(.system(size:14))
+                                .foregroundColor(Color("ColorFontGray"))
+                            Spacer()
+                            Button{
+                                
+                            } label: {
+                                Text("설정")
+                                    .font(.system(size:13))
+                                    .foregroundColor(Color("ColorFontOrange"))
+                                    .padding(.horizontal, 9)
+                                    .padding(.vertical, 6)
+                                    .background(Color("ColorBgGrayList"))
+                                    .cornerRadius(30)
+                            }
+                        }
+                    }
+            // 기타 아래 Section
+            Section(
+                header:
+                    HStack{
+                        Text("기타")
+                            .font(.system(size: 17))
+                            .foregroundColor(Color("ColorFontWhite"))
+                    }
+            ){
                 Text("asdf")
                 Text("asdf")
             }
         }
         .listStyle(.plain)
         .toolbar {
-            Image(systemName: "plus")
-                .foregroundColor(Color("ColorIconOrange"))
-                .padding(.vertical, 10)
-                .padding(.trailing, 16)
+            Button{
+                isPlusPressed.toggle()
+            } label: {
+                Image(systemName: "plus")
+                    .foregroundColor(Color("ColorIconOrange"))
+                    .padding(.vertical, 10)
+                    .padding(.trailing, 16)
+            }
         }
         .navigationTitle("알람")
+        .sheet(isPresented: $isPlusPressed) {
+            NavigationView {
+                AlarmSettingView(isPlusPressed: $isPlusPressed)
+            }
+        }
     }
     
 }
