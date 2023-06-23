3c4b6912a8d4c9ba2fa0c3c7d7a0348da7dff5d3
jay1261 <dongjae73@naver.com>
2023-06-23 09:01:52 +0900

    [Add] - act4 init

---
+++ act4/act4/act4App.swift
@@ -0,0 +1,17 @@
+//
+//  act4App.swift
+//  act4
+//
+//  Created by Jay on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct act4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
